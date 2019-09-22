5b25cd01f9a4a4aa3f64774c76a8259b786b9ef9
ribelli <ribelli.oi@gmail.com>
2019-09-22 23:05:10 +0300

    without a nice commit-texts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          <a href="http://google.com">hjkhjh</a>
       </header>
     </div>
   );
